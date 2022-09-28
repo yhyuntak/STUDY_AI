3d8c6453937a6aaddd5dfe1092fd39205e17b246
YooHyuntak <yhyuntak@gmail.com>
2022-09-28 15:08:50 +0000

    update commit ipynb

---
+++ Tensorflow/책_딥_러닝을_이용한_자연어_처리_입문/CH2/section2_정제_정규화.py
@@ -0,0 +1,6 @@
+import re
+text = "I was wondering if anyone out there could enlighten me on this car."
+
+# 길이가 1~2인 단어들을 정규 표현식을 이용하여 삭제
+shortword = re.compile(r'\W*\b\w{1,2}\b')
+print(shortword.sub('',text))
