cc3e7d3c5b149bdd2b76a89d09e87ff10a3b71d8
yhyuntak <yhyuntak@gmail.com>
2022-09-28 21:25:18 +0900

    220927

--- Tensorflow/책_딥_러닝을_이용한_자연어_처리_입문/CH1/section5.py
+++ Tensorflow/책_딥_러닝을_이용한_자연어_처리_입문/CH1/section5.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pandas_profiling
-data =pd.read_csv('../dataset/spam.csv',encoding='latin1')
+data =pd.read_csv('../dataset/spam.csv', encoding='latin1')
 print(data.head(5))
 
 pr = data.profile_report()
